e90cc08b755b96ef892e4fb25d43f3b25d89fae8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-10-14 00:48:37 -0400

    tests: Fix python_version_check on python 3

--- _tests/python_check_version.py
+++ _tests/python_check_version.py
@@ -5,7 +5,8 @@ import sys
 current_version = list(sys.version_info[:3])
 print("current_version: %s" % str(current_version))
 
-expected_version = map(int, os.environ["EXPECTED_PYTHON_VERSION"].split("."))
+expected_version = list(
+    map(int, os.environ["EXPECTED_PYTHON_VERSION"].split(".")))
 print("expected_version: %s" % str(expected_version))
 
 assert current_version == expected_version
