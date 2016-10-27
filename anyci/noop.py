7e8fa3bb05262cd5745ed8c08870dfaf75348436
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-10-27 02:16:03 -0400

    ci: Add anyci/noop addon useful for testing

---
+++ anyci/noop.py
@@ -0,0 +1,27 @@
+
+"""
+Usage::
+
+    import noop
+    noop.execute()
+
+"""
+
+import os
+import sys
+
+
+def _log(*args):
+    script_name = os.path.basename(__file__)
+    print("[generic:%s] " % script_name + " ".join(args))
+    sys.stdout.flush()
+
+
+def execute():
+    """Display value of ``sys.argv`` and exit.
+    """
+    _log("Display value of 'sys.argv'")
+    print(" ".join(sys.argv))
+
+if __name__ == '__main__':
+    execute()
