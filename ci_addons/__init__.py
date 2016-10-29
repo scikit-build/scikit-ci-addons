1ecbb6af65d0ed188f184d1ef4faa1cf05d79076
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-10-29 01:16:53 -0400

    ci_addons: Fix spelling of add-on(s) in docstrings
    [ci skip]

--- ci_addons/__init__.py
+++ ci_addons/__init__.py
@@ -17,7 +17,7 @@ __version__ = '0.5.0'
 
 
 def addons():
-    """Return all available addons."""
+    """Return all available add-ons."""
 
     addons = []
 
@@ -40,14 +40,14 @@ def addons():
 
 
 def home():
-    """Return directory where all addons can be found."""
+    """Return directory where all add-ons can be found."""
     return os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 def path(addon_name):
     """Return path of ``addon_name``.
 
-    This is particularly useful when the selected addon is not a python
+    This is particularly useful when the selected add-on is not a python
     script and is expected to be used an input to an other tool."""
 
     addon_path = tmp_addon_path = os.path.join(home(), addon_name)
@@ -59,9 +59,9 @@ def path(addon_name):
 
 
 def install(dst_path, force=False):
-    """Copy addons into ``dst_path``.
+    """Copy add-ons into ``dst_path``.
 
-    By default, existing addons are *NOT* overwritten. Specifying ``force``
+    By default, existing add-ons are *NOT* overwritten. Specifying ``force``
     allow to overwrite them.
     """
     dst_path = os.path.normpath(os.path.abspath(dst_path))
@@ -90,7 +90,7 @@ def install(dst_path, force=False):
 def execute(addon_name, arguments=[]):
     """Execute ``addon_name`` with ``arguments``.
 
-    Executable addons are python script.
+    Executable add-ons are python script.
     """
     cmd = [sys.executable, path(addon_name)] + arguments
     check_call(cmd)
