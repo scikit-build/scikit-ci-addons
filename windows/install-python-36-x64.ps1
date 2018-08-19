7f46b31f85428fb7cb9ff07e8d76be90a790fea1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-08-19 18:02:41 -0400

    addons/windows: Download "install-utils.ps1" into "C:/Downloads"

--- windows/install-python-36-x64.ps1
+++ windows/install-python-36-x64.ps1
@@ -1,11 +1,13 @@
 trap { Write-Error $_; Exit 1 }
 
+$downloadDir = "C:/Downloads"
+New-Item -ItemType Directory -Force -Path $downloadDir
+
 $scriptName = "install-python.ps1"
-if (![System.IO.File]::Exists(".\$scriptName")) {
+if (![System.IO.File]::Exists("$downloadDir\$scriptName")) {
   Write-Host "Download $scriptName"
   $url = "https://raw.githubusercontent.com/scikit-build/scikit-ci-addons/master/windows/$scriptName"
-  $cwd = (Get-Item -Path ".\" -Verbose).FullName
-  (new-object net.webclient).DownloadFile($url, "$cwd\$scriptName")
+  (new-object net.webclient).DownloadFile($url, "$downloadDir\$scriptName")
 }
 
 $pythonPrependPath = "1"
