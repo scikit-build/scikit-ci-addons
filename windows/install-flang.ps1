cba292494a3359d59b15a9a125a67130c62a0c27
sjh26 <sam.horvath@kitware.com>
2019-08-05 14:20:11 -0400

    WIP

--- windows/install-flang.ps1
+++ windows/install-flang.ps1
@@ -6,7 +6,7 @@ if(!(Test-Path -Path $CONDA_ROOT )){
   throw "Creating 'flang-env' environment and installing 'flang' requires $CONDA_ROOT"
 }
 $env:path = "$CONDA_ROOT;$CONDA_ROOT\Scripts;$env:path"
-conda activate base
+conda activate
 conda config --set show_channel_urls yes
 conda config --prepend channels conda-forge
 conda create -q -y -n flang-env flang
