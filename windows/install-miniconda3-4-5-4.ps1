f305759bf19a97cadef296ba20680119e1a529ab
Sam Horvath <sam.horvath@kitware.com>
2019-08-08 13:39:12 -0400

    ENH:  Add script for Miniconda3 4.5.4 Win64

---
+++ windows/install-miniconda3-4-5-4.ps1
@@ -0,0 +1,18 @@
+trap { Write-Error $_; Exit 1 }
+
+$downloadDir = "C:/Downloads"
+New-Item -ItemType Directory -Force -Path $downloadDir
+
+if (![System.IO.File]::Exists("$downloadDir\install-utils.ps1")) {
+  Write-Host "Download install-utils.ps1"
+  $url = "https://raw.githubusercontent.com/scikit-build/scikit-ci-addons/master/windows/install-utils.ps1"
+  (new-object net.webclient).DownloadFile($url, "$downloadDir\install-utils.ps1")
+}
+Import-Module "$downloadDir\install-utils.ps1" -Force
+
+$archiveName = "Miniconda3-4.5.4-Windows-x86_64.exe"
+
+Download-URL "https://repo.continuum.io/miniconda/$archiveName" $downloadDir
+
+$installer = Join-Path $downloadDir $archiveName
+Start-Process $installer -ArgumentList "/InstallationType=JustMe /AddToPath=0 /NoRegistry=1 /RegisterPython=0 /S /D=C:\Miniconda3" -NoNewWindow -Wait
