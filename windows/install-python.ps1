scikit-build/scikit-ci-addons

commit eaecd9ef49b3ad9fa21d757ff2984b2c0459f229

windows/install-python: Support intallation of Python 3.8

windows/install-python.ps1
@@ -3,7 +3,7 @@ trap { Write-Error $_; Exit 1 }
 #
 # By default, all version of python are installed.
 #
-# Setting $pythonVersion to "2.7", "3.4", "3.5", "3.6" or "3.7" allows to install a specific version
+# Setting $pythonVersion to "2.7", "3.4", "3.5", "3.6", "3.7" or "3.8" allows to install a specific version
 #
 # Setting $pythonArch to either "64" or "86" allows to install python for specific architecture.
 #
@@ -192,7 +192,7 @@ if(!($pythonPrependPath -match "^(0|1)$")){
 #
 # Python 2.7 and 3.4
 #
-# * 3.4.4 is last version released in binary form
+# * 3.4.4 is last 3.4.x version released in binary form
 #
 $exeVersions = @("2.7.15", "3.4.4")
 foreach ($version in $exeVersions) {
@@ -228,15 +228,17 @@ foreach ($version in $exeVersions) {
 }
 
 #
-# Python 3.5, 3.6, and 3.7
+# Python 3.5, 3.6, 3.7 and 3.8
 #
-# * 3.5.4 is last version released in binary form
-$exeVersions = @("3.5.4", "3.6.7", "3.7.2")
+# * 3.5.4 is last 3.5.x version released in binary form
+#
+$exeVersions = @("3.5.4", "3.6.7", "3.7.2", "3.8.0a2")
 foreach ($version in $exeVersions) {
 
   $split = $version.Split(".")
   $majorMinor = [string]::Join("", $split, 0, 2)
   $majorMinorDot = [string]::Join(".", $split, 0, 2)
+  $xyzVersion = [regex]::Replace($version, "(\d+\.\d+\.\d+).+", '$1')
 
   if($pythonVersion -And ! $pythonVersion.CompareTo($majorMinor) -eq 0) {
     Write-Host "Skipping $majorMinor"
@@ -248,7 +250,7 @@ foreach ($version in $exeVersions) {
   #
   if (!$pythonArch -Or $pythonArch.CompareTo("64") -eq 0) {
 
-    Download-URL "https://www.python.org/ftp/python/$($version)/python-$($version)-amd64.exe" $downloadDir
+    Download-URL "https://www.python.org/ftp/python/$($xyzVersion)/python-$($version)-amd64.exe" $downloadDir
 
     $pythonInstallPath = Get-Python-InstallPath $majorMinorDot "64"
     $targetInstallPath = "C:\Python$($majorMinor)-x64\"
@@ -287,7 +289,7 @@ foreach ($version in $exeVersions) {
   # 32-bit
   #
   if (!$pythonArch -Or $pythonArch.CompareTo("86") -eq 0) {
-    Download-URL "https://www.python.org/ftp/python/$($version)/python-$($version).exe" $downloadDir
+    Download-URL "https://www.python.org/ftp/python/$($xyzVersion)/python-$($version).exe" $downloadDir
 
     $pythonInstallPath = Get-Python-InstallPath $majorMinorDot "86"
     $targetInstallPath = "C:\Python$($majorMinor)-x86\"

commit dea78e61f2b49cae5b9bc23389d5a569181df985

windows/install-python: Fix installation of version with 2-digit patch number
Fix regression originally introduced in eaecd9ef4 (Support installation
of Python 3.8) adding support for version strings like "3.8.0a2".

The issue was exerted following f277449bc (install-python.ps1: Add 3.11
and bump earlier versions)

Summary:

| $version |   $xyzVersion   |
|----------|--------|--------|
| input    | before | after  |
|----------|--------|--------|
| 3.8.0    | 3.8.0  | 3.8.0  |
| 3.8.0a2  | 3.8.0  | 3.8.0  |
| 3.8.11a2 | 3.8.11 | 3.8.11 |
| 3.8.11   | 3.8.1  | 3.8.11 |

Co-authored-by: Tom Birdsong <tom.birdsong@kitware.com>

windows/install-python.ps1
@@ -199,7 +199,7 @@ foreach ($version in $exeVersions) {
   $split = $version.Split(".")
   $majorMinor = [string]::Join("", $split, 0, 2)
   $majorMinorDot = [string]::Join(".", $split, 0, 2)
-  $xyzVersion = [regex]::Replace($version, "(\d+\.\d+\.\d+).+", '$1')
+  $xyzVersion = [regex]::Replace($version, "(\d+\.\d+\.\d+).*", '$1')
 
   if($pythonVersion -And ! $pythonVersion.CompareTo($majorMinor) -eq 0) {
     Write-Host "Skipping $majorMinor"