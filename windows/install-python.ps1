scikit-build/scikit-ci-addons

commit f277449bc6d9f3d4f9ad3cd33ba46e6c2bf26d3a

install-python.ps1: Add 3.11 and bump earlier versions

windows/install-python.ps1
@@ -3,7 +3,7 @@ trap { Write-Error $_; Exit 1 }
 #
 # By default, all version of python are installed.
 #
-# Setting $pythonVersion to "2.7", "3.6", "3.7", "3.8", "3.9", "3.10" allows to install a specific version
+# Setting $pythonVersion to "2.7", "3.6", "3.7", "3.8", "3.9", "3.10", "3.11" allows to install a specific version
 #
 # Setting $pythonArch to either "64", "86" or "32" allows to install python for specific architecture.
 # Values "86" and "32" correspond to the same architecture.
@@ -227,9 +227,9 @@ foreach ($version in $exeVersions) {
 }
 
 #
-# Python 3.6, 3.7, 3.8, 3.9, 3.10
+# Python 3.6, 3.7, 3.8, 3.9, 3.10, 3.11
 #
-$exeVersions = @("3.6.8", "3.7.2", "3.8.6", "3.9.4", "3.10.0")
+$exeVersions = @("3.6.8", "3.7.9", "3.8.10", "3.9.13", "3.10.8", "3.11.0")
 foreach ($version in $exeVersions) {
 
   $split = $version.Split(".")

commit a849cb886abe0f7feb38809792e852b1d8ff475d

ci/github-actions: Fix expected python versions
The issue was introduced in f277449bc (install-python.ps1: Add 3.11
and bump earlier versions)

.github/workflows/CI-windows-addons.yml
@@ -18,16 +18,16 @@ jobs:
             expected: "3.6.8"
 
           - version: "3.7"
-            expected: "3.7.2"
+            expected: "3.7.9"
 
           - version: "3.8"
-            expected: "3.8.6"
+            expected: "3.8.10"
 
           - version: "3.9"
-            expected: "3.9.4"
+            expected: "3.9.13"
 
           - version: "3.10"
-            expected: "3.10.0"
+            expected: "3.10.8"
 
           - version: "3.11"
             expected: "3.11.0"

windows/install-python.ps1
@@ -193,6 +193,11 @@ if(!($pythonPrependPath -match "^(0|1)$")){
 #
 # Python 3.6, 3.7, 3.8, 3.9, 3.10, 3.11
 #
+# Maintainer:
+#
+# - If you add/remove/update python versions, consider updating the
+#   file `.github/workflows/CI-windows-addons.yml`.
+#
 $exeVersions = @("3.6.8", "3.7.9", "3.8.10", "3.9.13", "3.10.8", "3.11.0")
 foreach ($version in $exeVersions) {
 